aba0715eb4c6768e4749b33831254745f6bf7aad
Ishwar <ishwartrada15@gmail.com>
2025-01-12 18:58:58 +0530

    Authentication API and middleware, cloudinary,mailtrap configuration done

---
+++ backend/routes/index.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import userRoutes from "./user.routes.js";
+
+const router = Router();
+
+router.use("/api/v1/user", userRoutes);
+
+export default router;
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,20 @@
+import e, { Router } from "express";
+import {
+  createUser,
+  forgotPassword,
+  loginUser,
+  logout,
+  resetPassword,
+  verifyOTP,
+} from "../controllers/user/user.acontroller.js";
+
+const router = Router();
+
+router.post("/signup", createUser);
+router.post("/verify-otp", verifyOTP);
+router.post("/login", loginUser);
+router.post("/logout", logout);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:resetToken", resetPassword);
+
+export default router;
